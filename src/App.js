1d25b793850b61c4df20c804d930c9f3ebd7c2e4
Kaustubh <kns.ravindra@gmail.com>
2020-10-02 16:55:01 +0530

    Shop page added to project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import React from 'react';
-// import {Route, Switch} from 'react-router-dom';
-import Homepage from './pages/homePage/homepage.component';
-import './App.css';
-
-export const SamplePage = () =>{
-  return(
-    <div>This is a sample page</div>
-  );
-}
+import React from "react";
+import {Route, Switch} from 'react-router-dom';
+import Homepage from "./pages/homePage/home.component";
+import ShopPage from "./pages/shopPage/shop.component";
+import "./App.css";
 
 function App() {
   return (
-    <div><Homepage></Homepage></div>
-    // <div>
-    //   <Switch>
-    //     <Route path="/" exact component={Homepage}></Route>
-    //     <Route path="/sample" component={SamplePage}></Route>
-    //   </Switch>
-    // </div>
+    // <div><Homepage></Homepage></div>
+    <div>
+      <Switch>
+        <Route path="/" exact component={Homepage}></Route>
+        <Route path="/shop" component={ShopPage}></Route>
+      </Switch>
+    </div>
   );
 }
 
--- src/pages/homePage/home.component.js
+++ src/pages/homePage/home.component.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Directory from "../../components/directory/directory.component";
 
-import "./homepage.component.scss";
+import "./home.component.scss";
 
 export const HomePage = () => {
   return (
